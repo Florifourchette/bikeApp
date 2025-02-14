406900ac9b55f1ca5cb66d43418a24efeb3b1f6c
f.michelat <f.michelat@reply.de>
2025-02-14 11:57:04 +0100

    add authentication, it is also working on Postman

--- src/main/java/com/bike/app/bikeApp/config/SecurityConfig.java
+++ src/main/java/com/bike/app/bikeApp/config/SecurityConfig.java
@@ -1,39 +1,23 @@
 package com.bike.app.bikeApp.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/**").hasRole("USER").and().oauth2Login(oauth2 -> oauth2
-                .defaultSuccessUrl("/", true)
-                .userInfoEndpoint(userInfo -> userInfo
-                        .userAuthoritiesMapper(authorities -> {
-                            // Log the original authorities to debug
-                            authorities.forEach(authority -> System.out.println("Authority: " + authority.getAuthority()));
-
-                            // Map authorities to Spring Security roles
-                            return authorities.stream()
-                                    .map(grantedAuthority -> {
-                                        if ("OAUTH2_USER".equals(grantedAuthority.getAuthority()) || "SCOPE_read:user".equals(grantedAuthority.getAuthority())) {
-                                            // If the user has these authorities, map to ROLE_USER
-                                            return new SimpleGrantedAuthority("ROLE_USER");
-                                        }
-                                        return grantedAuthority; // Keep other authorities as they are
-                                    })
-                                    .collect(Collectors.toList());
-                        })
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
                 )
-        );
-
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/", true)
+                );
 
         return http.build();
     }
